bf2de873cb9b2b917b804d68ab70d09096ef2a28
Seung-Jong Oh <71041956+tmdwhd1213@users.noreply.github.com>
2022-02-16 16:29:20 +0900

    Delete ModernJS/6_DataType directory

--- ModernJS/6_DataType/templateL.js
+++
@@ -1,7 +0,0 @@
-// 보통 줄바꿈할때 /n, 탭을 할때 /t를 쓰는 등 이스케이프 문자를 쓰게 되는데
-//템플릿 리터럴(ES6+)을 사용하면 표현하고 싶은 대로 하면 된다.
-var template = `<ul>
-    <li><a href="#">Home</a></li>
-</ul>`;
-
-console.log(template);
--- ModernJS/6_DataType/undefined.js
+++
@@ -1,6 +0,0 @@
-// var 키워드로 선언한 변수는 암묵적으로 undefined로 초기화된다. 
-// 변수 선언에 의해 확보된 메모리 공간을 처음 할당이 이뤄질 때까지 빈 상태(garbage value가 들어있다.)로 내버려두지 않고 JS엔진이 undefined로 초기화한다.
-// 변수 선언 후 값을 할당하지 않은 변수를 참조하면 undefined가 반환된다.
-
-var foo;
-console.log(foo);   // undefined(정의되지 않은, 공백(null)이랑은 다르다.)
